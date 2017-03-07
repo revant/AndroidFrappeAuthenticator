dfb2b5f760deeec2ce0790b5b517edd0d019ddaa
Revant Nandgaonkar <revant.one@gmail.com>
2017-03-07 23:43:54 +0530

    WIP Sync Calendar

---
+++ app/src/main/java/io/frappe/frappeauthenticator/sync/EventsHelper.java
@@ -0,0 +1,235 @@
+package io.frappe.frappeauthenticator.sync;
+
+/**
+ * Created by revant on 1/3/17.
+ */
+
+import android.accounts.Account;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.ContentResolver;
+import android.content.OperationApplicationException;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.RawContacts;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class EventsHelper {
+
+    public static void addEvent(Account account, JSONObject contactInfo, ContentResolver mContentResolver) {
+
+        String contactName = null;
+        String supplierName = null;
+        String customerName = null;
+        String salePartnerName = null;
+        String lastName = null;
+        String emailID = null;
+        String mobileNo = null;
+        String firstName = null;
+        String department = null;
+        String designation = null;
+        String phone = null;
+        String displayName = null;
+
+        try {
+            contactName = contactInfo.getString("name");
+            customerName = contactInfo.getString("customer_name");
+            supplierName = contactInfo.getString("supplier_name");
+            salePartnerName = contactInfo.getString("sales_partner");
+            lastName = contactInfo.getString("last_name");
+            emailID = contactInfo.getString("email_id");
+            mobileNo = contactInfo.getString("mobile_no");
+            firstName = contactInfo.getString("first_name");
+            department = contactInfo.getString("department");
+            designation = contactInfo.getString("designation");
+            phone = contactInfo.getString("phone");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (!firstName.equals("null") && !lastName.equals("null")){
+            displayName = firstName + " " + lastName;
+        }
+        else if (!firstName.equals("null") && lastName.equals("null")){
+            displayName = firstName;
+        }
+        else if (!lastName.equals("null") && firstName.equals("null")){
+            displayName = lastName;
+        }
+
+        //Create our RawContact
+        ArrayList<ContentProviderOperation> op_list = new ArrayList<ContentProviderOperation>();
+        op_list.add(ContentProviderOperation.newInsert(addCallerIsSyncAdapterParameter(
+                RawContacts.CONTENT_URI, true))
+                .withValue(RawContacts.ACCOUNT_TYPE, account.type)
+                .withValue(RawContacts.ACCOUNT_NAME, account.name)
+                .withValue(RawContacts.RAW_CONTACT_IS_READ_ONLY,"1")
+                .withValue(RawContacts.SYNC1,contactName)
+                .withValue(RawContacts.AGGREGATION_MODE, RawContacts.AGGREGATION_MODE_DEFAULT)
+                .build());
+
+        // first and last names
+
+        if (displayName!=null && !displayName.isEmpty()){
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.StructuredName.RAW_CONTACT_ID, 0)
+                    .withValue(RawContacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, displayName)
+                    .build());
+        }
+
+        if (!firstName.equals("null")){
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.StructuredName.RAW_CONTACT_ID, 0)
+                    .withValue(RawContacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, firstName)
+                    .build());
+        }
+
+        if (!lastName.equals("null")){
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.StructuredName.RAW_CONTACT_ID, 0)
+                    .withValue(RawContacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME, lastName)
+                    .build());
+        }
+        // add phone number
+        if (!phone.equals("null")) {
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(Phone.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
+                    .withValue(Phone.NUMBER, phone)
+                    .withValue(Phone.TYPE, Phone.TYPE_WORK)
+                    .build());
+        }
+
+        //add mobile number
+        if (!mobileNo.equals("null")) {
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(Phone.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
+                    .withValue(Phone.NUMBER, mobileNo)
+                    .withValue(Phone.TYPE, Phone.TYPE_MOBILE)
+                    .build());
+        }
+
+        //add email
+        if (!emailID.equals("null")) {
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.Email.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Email.ADDRESS, emailID)
+                    .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                    .build());
+        }
+
+        //add Customer
+        if(!customerName.equals("null")){
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.Organization.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, customerName)
+                    .build());
+        }
+
+        //add Supplier
+        if(!supplierName.equals("null")){
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.Organization.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, supplierName)
+                    .build());
+        }
+
+        //add Sales Partner
+        if(!salePartnerName.equals("null")){
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.Organization.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, salePartnerName)
+                    .build());
+        }
+
+        //add Department
+        if(!department.equals("null")){
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.Organization.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Organization.DEPARTMENT, department)
+                    .build());
+        }
+
+        //add Designation
+        if(!designation.equals("null")){
+            op_list.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.CommonDataKinds.Organization.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, designation)
+                    .build());
+        }
+
+        op_list.add(ContentProviderOperation.newInsert(addCallerIsSyncAdapterParameter(ContactsContract.Data.CONTENT_URI, true))
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE, "vnd.android.cursor.item/vnd.io.frappe.frappeauthenticator.contact")
+                .withValue(ContactsContract.Data.DATA1, mobileNo)
+                .withValue(ContactsContract.Data.DATA2, displayName)
+                .withValue(ContactsContract.Data.DATA3, contactName)
+                .withValue(ContactsContract.Data.DATA4, phone)
+                .withValue(ContactsContract.Data.DATA5, emailID)
+                .withValue(ContactsContract.Data.DATA6, firstName)
+                .withValue(ContactsContract.Data.DATA7, lastName)
+                .withValue(ContactsContract.Data.DATA8, designation)
+                .withValue(ContactsContract.Data.DATA9, department)
+                .withValue(ContactsContract.Data.DATA10, customerName)
+                .withValue(ContactsContract.Data.DATA11, supplierName)
+                .withValue(ContactsContract.Data.DATA12, salePartnerName)
+                .build());
+
+        try{
+            ContentProviderResult[] results = mContentResolver.applyBatch(ContactsContract.AUTHORITY, op_list);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateEvent(Account account, JSONObject contactInfo, ContentResolver mContentResolver) {
+
+    }
+
+    public void deleteContact(Account account, String name, ContentResolver mContentResolver) {
+
+        String where = RawContacts.SYNC1 + " = ? AND " + RawContacts.ACCOUNT_TYPE + " = ? AND " + RawContacts.ACCOUNT_NAME + " = ? ";
+        String[] params = new String[] {name, account.type, account.name};
+
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        ops.add(ContentProviderOperation.newDelete(addCallerIsSyncAdapterParameter(
+                RawContacts.CONTENT_URI, true))
+                .withSelection(where, params)
+                .build());
+        try {
+            mContentResolver.applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (OperationApplicationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private static Uri addCallerIsSyncAdapterParameter(Uri uri, boolean isSyncOperation) {
+        if (isSyncOperation) {
+            return uri.buildUpon()
+                    .appendQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER,
+                            "true").build();
+        }
+        return uri;
+    }
+
+}
---
+++ app/src/main/java/io/frappe/frappeauthenticator/sync/EventsSyncAdapterService.java
@@ -0,0 +1,158 @@
+package io.frappe.frappeauthenticator.sync;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.app.Service;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.content.SyncResult;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.provider.BaseColumns;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import io.frappe.frappeauthenticator.authenticator.AccountGeneral;
+
+/**
+ * Created by revant on 26/2/17.
+ */
+
+public class EventsSyncAdapterService extends Service {
+    private static final String TAG = "FrappeSync";
+    private static SyncAdapterImpl sSyncAdapter = null;
+    private static ContentResolver mContentResolver = null;
+
+    public EventsSyncAdapterService() {
+        super();
+    }
+
+    private static class SyncAdapterImpl extends AbstractThreadedSyncAdapter {
+        private Context mContext;
+
+        public SyncAdapterImpl(Context context) {
+            super(context, true);
+            mContext = context;
+        }
+
+        @Override
+        public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+            try {
+                EventsSyncAdapterService.performSync(mContext, account, extras, authority, provider, syncResult);
+            } catch (OperationCanceledException e) {
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        IBinder ret = null;
+        ret = getSyncAdapter().getSyncAdapterBinder();
+        return ret;
+    }
+
+    private SyncAdapterImpl getSyncAdapter() {
+        if (sSyncAdapter == null)
+            sSyncAdapter = new SyncAdapterImpl(this);
+        return sSyncAdapter;
+    }
+
+    private static void performSync(Context context, final Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult)
+            throws OperationCanceledException {
+        mContentResolver = context.getContentResolver();
+
+        Log.i("Frappe Sync: ", account.toString());
+        AccountManager am = AccountManager.get(context);
+        String frappeServer = am.getUserData(account, "frappeServer");
+        String authToken = null;
+        try {
+            authToken = am.blockingGetAuthToken(account, AccountGeneral.AUTHTOKEN_TYPE_FULL_ACCESS, true);
+            ArrayList<ContentProviderOperation> operationList = new ArrayList<ContentProviderOperation>();
+            try {
+                JSONObject bearerToken = null;
+                FrappeEventProvider server = new FrappeEventProvider();
+                try {
+                    bearerToken = new JSONObject(authToken);
+                    server.getEvents(frappeServer,bearerToken.getString("access_token"),new FrappeServerCallback() {
+                        @Override
+                        public void onSuccessJSONObject(JSONObject response) {
+
+                            // Load the local app contacts
+                            ArrayList<ContentProviderOperation> operationList = new ArrayList<ContentProviderOperation>();
+                            HashMap<String, Long> localContacts = new HashMap<String, Long>();
+                            try{
+                                Uri rawContactUri = ContactsContract.RawContacts.CONTENT_URI.buildUpon().appendQueryParameter(ContactsContract.RawContacts.ACCOUNT_NAME, account.name).appendQueryParameter(
+                                        ContactsContract.RawContacts.ACCOUNT_TYPE, account.type).build();
+                                Cursor c1 = mContentResolver.query(rawContactUri, new String[] { BaseColumns._ID, ContactsContract.RawContacts.SYNC1 }, null, null, null);
+                                while (c1.moveToNext()) {
+                                    localContacts.put(c1.getString(1), c1.getLong(0));
+                                }
+                            }
+                            catch (Exception e){
+                                e.printStackTrace();
+                            }
+
+                            JSONArray contactsList = new JSONArray();
+                            try {
+                                contactsList = response.getJSONArray("data");
+                                for(int i=0;i<contactsList.length();i++){
+                                    JSONObject object = null;
+                                    try {
+                                        object = contactsList.getJSONObject(i);
+                                        EventsHelper eHelper = new EventsHelper();
+                                        if (!localContacts.containsKey(object.getString("name"))) {
+                                            eHelper.addEvent(account, object, mContentResolver);
+                                        }
+                                        else{
+                                            //cHelper.deleteContact(account, object.getString("name"), mContentResolver);
+                                            eHelper.updateEvent(account, object, mContentResolver);
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                if(operationList.size() > 0)
+                                    mContentResolver.applyBatch(ContactsContract.AUTHORITY, operationList);
+                                System.out.println(contactsList.toString());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (RemoteException e) {
+                                e.printStackTrace();
+                            } catch (OperationApplicationException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } catch (Exception e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (AuthenticatorException e) {
+            e.printStackTrace();
+        }
+    }
+}
